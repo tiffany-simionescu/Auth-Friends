9c98bb59be03ce63b71dd56455e874f84a626c64
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-12-03 22:19:25 -0500

    Updated PrivateRoute, added FriendList component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import FriendList from './components/FriendList';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>
+            <Link to="/login">Log in</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
       </header>
     </div>
   );
