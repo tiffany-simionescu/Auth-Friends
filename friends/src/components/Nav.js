ca881bd470b708b074325b0797a0abff2c884530
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-12-06 15:43:55 -0500

    Added Logout functionality

--- friends/src/components/Nav.js
+++ friends/src/components/Nav.js
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <div className="nav">
-      <Link to="/login" className="link">Log in</Link>
       <Link to="/friendlist" className="link">Friend List</Link>
+      <Link to="/login" className="link">Log in</Link>
+      <Link to="/login" className="link" 
+        onClick={() => localStorage.clear()}>Log out</Link>
     </div>
   )
 }
